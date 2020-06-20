9bc6e06e6ff33c451db6de8d613012f69415b06b
Pablo <lpablouc9@gmail.com>
2020-06-20 11:33:42 -0600

    Select multiple, le falta la funcion con jquery

--- CoTEC-WebView/src/app/components/adminView/pathologies/create-pathology/create-pathology.component.ts
+++ CoTEC-WebView/src/app/components/adminView/pathologies/create-pathology/create-pathology.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-pathology.component.css']
 })
 export class CreatePathologyComponent implements OnInit {
-
+  symptoms=[]
   constructor() { }
 
   ngOnInit(): void {
